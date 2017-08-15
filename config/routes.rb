da9a425fc6bdb5a3e492553694bf343238b958da
Slade Sundar <slade@savoirapp.com>
2017-08-15 12:53:00 +0100

    finished controllers, views, and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  # root 'tasks#index'
+
+  # get '/show'/:id, to: 'tasks#show'
+
+  # get '/new', to: 'tasks#new'
+
+  # post '/', to: 'tasks#create'
+
+  # get '/:id/edit', to: 'tasks#edit'
+
+  # patch '/:id', to: 'task#update'
+
+  # get '/:id', to: 'task#destroy'
+
+  resources :tasks
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
